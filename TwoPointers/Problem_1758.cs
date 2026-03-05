3515fa72fc6309944725efd8e2951e650a0b51a3
Anil K <anilk@Anils-MacBook-Air.local>
2026-03-05 20:55:54 +0530

    1758

---
+++ TwoPointers/Problem_1758.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int MinOperations(string s) {
+
+        int eops=0;
+        int oops=0;
+
+        bool fOdd=true;
+        for(int i=0;i<s.Length;i++)
+        {
+            bool isEven=s[i]=='0'?true:false;
+            if(fOdd==isEven)
+            eops+=1;
+            else
+            oops+=1;
+            fOdd=!fOdd;
+        }
+        return Math.Min(oops,eops);
+        
+    }
+}
