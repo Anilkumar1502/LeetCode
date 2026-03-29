f85a5b5340fc22084d2d518ca254199879a7149f
Anil K <anilkumarkancharapu28@gmail.com>
2026-03-29 14:22:44 +0530

    Check if Strings Can be Made Equal With Operations II

---
+++ Miscellaneous/Problem_2840.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public bool CheckStrings(string s1, string s2) {
+        
+        int n=s1.Length;
+        (int p1,int p2)[] parity=new (int,int)[26];
+
+        for(int i=0;i<n;i++){
+            int idx=s1[i]-'a';
+            if(i%2==1)
+            parity[idx].p1+=1;
+            else
+            parity[idx].p2+=1;
+        }
+
+        for(int i=0;i<n;i++){
+            int idx=s2[i]-'a';
+            var tuple=parity[idx];
+            if(i%2==1)
+            tuple.p1-=1;
+            else
+            tuple.p2-=1;
+
+            parity[idx]=tuple;
+            if(tuple.p1<0||tuple.p2<0)
+            return false;
+        }
+
+        // for(int i=0;i<26;i++){
+        //     if(parity[i].p1!=0||parity[i].p2!=0){
+        //     return false;
+        //     }
+        // }
+        return true;
+    }
+}
