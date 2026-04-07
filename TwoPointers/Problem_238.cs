d917d6cbd8c0aae36a76dcd9b7ab5fa24430341e
Anil K <anilkumarkancharapu28@gmail.com>
2026-04-07 23:46:35 +0530

    Product of Array Except Self

--- TwoPointers/Problem_238.cs
+++ TwoPointers/Problem_238.cs
@@ -1,21 +1,46 @@
+public interface IProductCalculator<T> {
+    T[] CalculateProduct(T[] items);
+    void PrintResult(T[] items);
+}
+
 public class Solution {
     public int[] ProductExceptSelf(int[] nums) {
+        int k = nums.Length;
+        int[] result = new int[k];
         
-        int n=nums.Length;
-        int[] result=new int[n];
-
-
-        int left=1;
-        int right=1;
-
-        for(int i=0;i<n;i++){
-           result[i]=left;
-           left*=nums[i];
+        result[0] = 1;
+        for(int i = 1; i < k; i++){
+            result[i] = result[i - 1] * nums[i - 1];
         }
-        for(int i=n-1;i>=0;i--){
-            result[i]*=right;
-            right*=nums[i];
+        
+        int suffix = 1;
+        for(int i = k - 1; i >= 0; i--){
+            result[i] *= suffix;
+            suffix *= nums[i];
         }
         return result;
+    }
+}
+
+public class SolutionWithPrint : Solution, IProductCalculator<int> {
+    public int[] CalculateProduct(int[] nums) {
+        return ProductExceptSelf(nums);
+    }
+    
+    public void PrintResult(int[] nums) {
+        int[] result = CalculateProduct(nums);
+        int index = 0;
+        int level = 1;
+        
+        while(index < result.Length) {
+            int spacing = (result.Length - level) * 2;
+            Console.Write(new string(' ', spacing));
+            
+            for(int i = 0; i < level && index < result.Length; i++) {
+                Console.Write(result[index++] + " ");
+            }
+            Console.WriteLine();
+            level++;
+        }
     }
 }
